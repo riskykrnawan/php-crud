92860e733da001b2c6b7f7052260e5397816cf8b
Risky Kurniawan <96241592+riskykrnawan@users.noreply.github.com>
2021-12-17 00:41:01 +0800

    👌👌

--- ubah.php
+++ ubah.php
@@ -36,7 +36,7 @@ if ( !isset($_SESSION["login"])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Riss</title>
     <link rel="icon" href="img/blackbullspng.png">
-    <link rel="stylesheet" href="bootstrap-4.5.3-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap-4.5.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body class="text-center">
